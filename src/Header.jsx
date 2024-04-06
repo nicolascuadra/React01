15187a34ce633a376d964fea536eeca7df8e9f55
nicolas cuadra <enc3062@hotmail.com>
2024-04-05 22:40:56 -0300

    ewt

---
+++ src/Header.jsx
@@ -0,0 +1,22 @@
+export function HeaderM(){
+    return(
+
+    <nav className="flex py-8 px-8 bg-gradient-to-r from-slate-400 to-slate-200">
+
+       <img alt='aca va un logo'  src='/Captura.JPG' className="size-24 rounded-3xl outline-white outline-double justify-start" />
+       <aside>
+        <strong>Mi cara</strong>
+        
+        
+        </aside> 
+
+
+
+
+
+    </nav>
+
+
+
+    )
+}
---
+++ src/pages/TresSimple.jsx
@@ -0,0 +1,16 @@
+
+
+function TresSimple() {
+  
+
+  return (
+     
+   <div className='bg-green-300 font-normal'>
+      
+         hola mundo con fondo verde;  esto me tomo como 3 hs! 
+      
+    </div>
+  )
+}
+
+export default TresSimple;
